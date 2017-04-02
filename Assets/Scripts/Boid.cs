46aa5fa67d5d7d191d06d3e49ebdf9998a743c40
tkapa <1005732@Student.sae.edu.au>
2017-04-02 19:28:20 +1000

    Flocking Simulation
    The flocking simulation is (mostly) complete. They're doing stuff with each other!

---
+++ Assets/Scripts/Boid.cs
@@ -0,0 +1,140 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Boid : MonoBehaviour
+{
+    public float movementSpeed = 0.2f;
+    public float maxRange = 2.0f;
+    public float minRange = 0.5f;
+    public float repellingForce = 1.5f;
+    public Vector3 moveToPos = new Vector3(0, 0, 0);
+
+    UI userInterface;
+
+    float xMin = -17.0f;
+    float xMax = 17.0f;
+    float yMin = -8.5f;
+    float yMax = 8.5f;
+
+    List<GameObject> surrBoids = new List<GameObject>();
+    
+    // Use this for initialization
+    void Start()
+    {
+        userInterface = GameObject.FindGameObjectWithTag("Manager").GetComponent<UI>();
+        Vector3 boidPosAvg = transform.position;
+        Vector3 boidMovAvg = transform.position;
+        Vector3 boidPushAvg = transform.position;
+        moveToPos = FindNextPos();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RestrictBoid();
+        FindBoidsInRange();
+
+        if(surrBoids.Count > 0)
+        {
+            foreach (GameObject b in surrBoids)
+            {
+                Debug.DrawLine(transform.position, b.transform.position, Color.green);
+            }
+
+            moveToPos = FindNextPos();
+        }
+
+        Debug.DrawLine(transform.position, moveToPos, Color.red);
+
+        if (Vector3.Distance(moveToPos, transform.position) > 0.5f)
+            transform.position = Vector3.MoveTowards(transform.position, moveToPos, movementSpeed * Time.deltaTime);
+        else
+            moveToPos = FindNextPos();
+
+        surrBoids.Clear();
+        UpdateValues(userInterface);
+    }
+
+    //Stop the boid from leaving the confines of the screen
+    void RestrictBoid()
+    {
+        if (transform.position.x > xMax)
+            transform.position = new Vector3(xMin + 2, transform.position.y, transform.position.z);
+        else if (transform.position.x < xMin)
+            transform.position = new Vector3(xMax - 2, transform.position.y, transform.position.z);
+
+        if (transform.position.y > yMax)
+            transform.position = new Vector3(transform.position.x, yMin + 2, transform.position.z);
+        else if (transform.position.y < yMin)
+            transform.position = new Vector3(transform.position.x, yMax - 2, transform.position.z);
+    }
+
+    void FindBoidsInRange()
+    {
+        if (GameObject.FindGameObjectsWithTag("Boid") != null)
+        {
+            foreach (GameObject b in GameObject.FindGameObjectsWithTag("Boid"))
+            {
+                float dist = Vector3.Distance(b.transform.position, transform.position);
+                if (dist < maxRange && dist > minRange)
+                    surrBoids.Add(b);
+                else
+                    continue;
+            }
+        }
+    }
+
+    void UpdateValues(UI ui)
+    {
+        movementSpeed = ui.moveSpeedV;
+        maxRange = ui.maxRangeV;
+        minRange = ui.minRangeV;
+    }
+
+    Vector3 FindNextPos()
+    {
+        Vector3 newPos = transform.position;
+
+        if (surrBoids.Count > 0)
+        {
+            newPos += AveragePos();
+            newPos += AverageMoveToPos();
+            //newPos += AverageSeparation();
+
+            newPos = new Vector3(newPos.x / 3, newPos.y / 3, 0);
+        }
+        else
+            newPos = new Vector3(Random.Range(-10, 10), Random.Range(-5, 5), 0);
+
+        return newPos;
+    }
+
+    Vector3 AveragePos()
+    {
+        Vector3 avgPos = new Vector3(0,0,0);
+
+        foreach(GameObject b in surrBoids)
+        {
+            avgPos += b.transform.position;
+        }
+
+        avgPos = new Vector3(avgPos.x / surrBoids.Count, avgPos.y / surrBoids.Count, 0);
+
+        return avgPos;
+    }
+
+    Vector3 AverageMoveToPos()
+    {
+        Vector3 avgPos = new Vector3(0, 0, 0);
+
+        foreach(GameObject b in surrBoids)
+        {
+            avgPos += b.GetComponent<Boid>().moveToPos;
+        }
+
+        avgPos = new Vector3(avgPos.x / surrBoids.Count, avgPos.y / surrBoids.Count, 0);
+
+        return avgPos;
+    }
+}
---
+++ Assets/Scripts/UI.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI : MonoBehaviour {
+
+    public GameObject Boid;
+    public Text boidCounter;
+
+    public Slider moveSpeedSlider;
+    public Slider maxRangeSlider;
+    public Slider minRangeSlider;
+
+    public float moveSpeedV;
+    public float maxRangeV;
+    public float minRangeV;
+
+    int boidCount;
+
+    void Start()
+    {
+        UpdateVars();
+        boidCount = GameObject.FindGameObjectsWithTag("Boid").Length;
+        boidCounter.text = "Boid Count: " + boidCount;
+    }
+
+    void Update()
+    {
+        UpdateVars();
+    }
+
+    public void SpawnBoid()
+    {
+        Vector3 spawnP = new Vector3(Random.Range(-16, 16), Random.Range(-8, 8), 0);
+        Instantiate(Boid, spawnP, transform.rotation);
+
+        boidCount += 1;
+        boidCounter.text = "Boid Count: " + boidCount;
+    }
+
+    public void SpawnTen()
+    {
+        for (int i = 0; i < 10; ++i)
+        {
+            SpawnBoid();
+        }
+    }
+
+    void UpdateVars()
+    {
+        moveSpeedV = moveSpeedSlider.value;
+        minRangeV = minRangeSlider.value;
+        maxRangeV = maxRangeSlider.value;
+    }
+}
